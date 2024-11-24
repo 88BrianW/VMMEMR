ed9720354b3940eb6c0abad3a2596d897ce2e268
88BrianW <88brianw@gmail.com>
2024-11-24 10:50:15 -0800

    host 0.0.0.0

--- server.py
+++ server.py
@@ -206,4 +206,4 @@ async def submit():
 
 if __name__ == "__main__":
     port_number = int(os.environ.get("PORT", 80))  # Default to 80 if PORT is not set
-    app.run(debug=True, use_reloader=True, port=port_number)
+    app.run(debug=True, use_reloader=True, port=port_number, host="0.0.0.0")
